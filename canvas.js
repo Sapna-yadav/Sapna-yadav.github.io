d10b26046e6672254598e39377d4cdbbf0ea8546
Sapna-yadav <sapna.yadav@prodigi.io>
2016-07-05 11:24:30 +0530

    initial commit

---
+++ canvas.js
@@ -0,0 +1,18 @@
+function draw() {
+var myCanvas = document.getElementById("myCanvas");
+var ctx = myCanvas.getContext("2d");
+var w = myCanvas.width, h = myCanvas.height;
+var i = 0;
+do {
+  ctx.fillStyle = "rgb(" + Math.round(255*Math.random()) + ","
+                  + Math.round(255*Math.random()) + ","
+                  + Math.round(255*Math.random()) + ")";
+  ctx.beginPath();
+  ctx.arc(w*Math.random(), h*Math.random(),
+          10*Math.random(),
+          0, Math.PI*2, true);
+  ctx.closePath();
+  ctx.fill();
+} while (++i != 1000);
+}
+  
